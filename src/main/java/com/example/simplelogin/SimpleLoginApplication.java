73835b4e3aea858164161258e6e7ae0c5f89041d
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-06 15:33:38 +0530

    Update SimpleLoginApplication.java

--- src/main/java/com/example/simplelogin/SimpleLoginApplication.java
+++ src/main/java/com/example/simplelogin/SimpleLoginApplication.java
@@ -1,4 +1,3 @@
-# src/main/java/com/example/simplelogin/SimpleLoginApplication.java
 package com.example.simplelogin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
