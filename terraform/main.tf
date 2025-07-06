e67e4622933917d78bb96d102faf517382238273
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-06 15:11:28 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,45 @@
 terraform {
-  required_providers { aws = { source="hashicorp/aws" } }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.3.0"
 }
-provider "aws" { region = var.region }
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "4.5.0"
-  name    = "${var.cluster_name}-vpc"
-  cidr    = "10.0.0.0/16"
-  azs     = data.aws_availability_zones.available.names[0:2]
-  public_subnets  = ["10.0.1.0/24","10.0.2.0/24"]
-  private_subnets = ["10.0.101.0/24","10.0.102.0/24"]
+  version = "6.0.1"
+
+  name = "${var.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names[0:2]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "21.0.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.37.1"
+
   cluster_name    = var.cluster_name
   cluster_version = "1.27"
-  vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.private_subnets
-  node_groups = {
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
     default = {
       desired_capacity = 2
       min_capacity     = 1
@@ -33,6 +52,3 @@ module "eks" {
 resource "aws_ecr_repository" "app" {
   name = var.ecr_repo
 }
-
-data "aws_availability_zones" "available" {}
-
