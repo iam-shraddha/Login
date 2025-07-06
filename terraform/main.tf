eeeb341d3271928e1733df8cff49f2f5a532e32b
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-06 15:46:03 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ module "vpc" {
   name = "${var.cluster_name}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = data.aws_availability_zones.available.names[0:2]
+  azs             = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
 
