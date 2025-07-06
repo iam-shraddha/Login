4946c1b04c24e4009d7d61ff4357ff8a7bf4ec0d
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-06 15:49:35 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,11 @@
-variable "region"       { type = string }
-variable "cluster_name" { type = string }
-variable "ecr_repo"     { type = string }
+variable "region" {
+  type = string
+}
 
+variable "cluster_name" {
+  type = string
+}
+
+variable "ecr_repo" {
+  type = string
+}
