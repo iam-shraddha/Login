340eeddd47ca6b6ae7eea6754768c5f8835d53c1
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-07 11:56:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,15 +16,17 @@ data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version =  "5.1.2" 
+  version = "5.1.2"
 
   name = "${var.cluster_name}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
+  azs             = data.aws_availability_zones.available.names
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
 
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
@@ -39,12 +41,22 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  manage_aws_auth_configmap = true
+
   eks_managed_node_groups = {
     default = {
       desired_capacity = 2
       min_capacity     = 1
       max_capacity     = 3
       instance_types   = ["t3.medium"]
+
+      create_iam_role = true
+
+      iam_role_additional_policies = [
+        "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+        "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+        "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+      ]
     }
   }
 }
